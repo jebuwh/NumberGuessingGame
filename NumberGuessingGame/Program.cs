cff9e9f083043cd9b8f24bcc7b5a88a921096be2
Mona Lisa <email@example.com>
2020-08-25 13:03:32 -0600

    added do while

--- NumberGuessingGame/Program.cs
+++ NumberGuessingGame/Program.cs
@@ -6,7 +6,6 @@ namespace NumberGuessingGame
 {
     class Program
     {
-        private static string favNumber;
 
         static void Main(string[] args)
         {
@@ -15,31 +14,31 @@ namespace NumberGuessingGame
                 var favNumber = r.Next(1, 1000);
                 Console.WriteLine("Guess the number I'm thinking of between 1 and 1,000:");
                 var userInput = int.Parse(Console.ReadLine());
-              
-        
-                {
-                  while (userInput != favNumber)
-                   {
-                        if (userInput < favNumber)
-                        {
-                            Console.WriteLine("Sorry, the number I am thinking of is higher than " + userInput + " Try and guess again!");
-                            Console.WriteLine("Guess Again:");
-                            userInput = int.Parse(Console.ReadLine());
-                        }
-                        else if (userInput > favNumber)
-                        {
-                            Console.WriteLine("Sorry, the number I am thinking of is lower than " + userInput + " Try and guess again!");
-                            Console.WriteLine("Guess Again:");
-                            userInput = int.Parse(Console.ReadLine());
-                        }
-                        else
-                            Console.WriteLine("Congrats, " + favNumber + " is correct!");
 
-                    }
+
+                {
+                    do
+                        if (userInput != favNumber)
+                        {
+                            if (userInput < favNumber)
+                            {
+                                Console.WriteLine("Sorry, the number I am thinking of is higher than " + userInput + " Try and guess again!");
+                                Console.WriteLine("Guess Again:");
+                                userInput = int.Parse(Console.ReadLine());
+                            }
+                            else if (userInput > favNumber)
+                            {
+                                Console.WriteLine("Sorry, the number I am thinking of is lower than " + userInput + " Try and guess again!");
+                                Console.WriteLine("Guess Again:");
+                                userInput = int.Parse(Console.ReadLine());
+                            }
+                        } while (userInput != favNumber);
                 }
-                
-                    
-                
+                if (userInput == favNumber)
+                     Console.WriteLine("Congrats, " + favNumber + " is correct!");
+
+
+
             }   
 
 
