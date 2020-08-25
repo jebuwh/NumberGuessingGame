4a51b2e3af143aaa7cc5fb3333c7ad90b34cff23
Mona Lisa <email@example.com>
2020-08-25 12:26:43 -0600

    fixed version

--- NumberGuessingGame/Program.cs
+++ NumberGuessingGame/Program.cs
@@ -11,26 +11,32 @@ namespace NumberGuessingGame
         static void Main(string[] args)
         {
             var r = new Random();
-            {   START:
+            {   
                 var favNumber = r.Next(1, 1000);
                 Console.WriteLine("Guess the number I'm thinking of between 1 and 1,000:");
                 var userInput = int.Parse(Console.ReadLine());
-
-                if (userInput != favNumber)
+              
+        
                 {
-                    if (userInput < favNumber)
+                    while (userInput != favNumber)
                     {
-                        Console.WriteLine("Sorry, the number I am thinking of is higher than " + userInput + " Try and guess again!");
-                        goto START;
+                        if (userInput < favNumber)
+                        {
+                            Console.WriteLine("Sorry, the number I am thinking of is higher than " + userInput + " Try and guess again!");
+                            Console.WriteLine("Guess Again:");
+                            userInput = int.Parse(Console.ReadLine());
+                        }
+                        else if (userInput > favNumber)
+                        {
+                            Console.WriteLine("Sorry, the number I am thinking of is lower than " + userInput + " Try and guess again!");
+                            Console.WriteLine("Guess Again:");
+                            userInput = int.Parse(Console.ReadLine());
+                        }
+
+
                     }
-                    else if (userInput > favNumber)
-                    {
-                        Console.WriteLine("Sorry, the number I am thinking of is lower than " + userInput + " Try and guess again!");
-                        goto START;
-                    }
-                    
                 }
-                if (userInput == favNumber)
+                if (userInput == favNumber);
                     Console.WriteLine("Congrats, " + favNumber + " is correct!");
                 
             }   
